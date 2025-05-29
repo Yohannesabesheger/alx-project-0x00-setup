d8a1c17f2a90500af5f462a8ae1f83ee0ab72681
yohannesm <jonmersha@gmail.com>
2025-05-29 09:18:37 +0300

    correction

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,8 +6,8 @@ const Landing: React.FC =  () => {
       <div>
         <h1 className=" text-xl font-extralight">Landing Page</h1>
         <Card />
-        <Button title="Click Me" styles="bg-blue-500 hover:bg-blue-700"  />
-      <Button title="Secondary Button" styles="bg-gray-500 hover:bg-gray-700" />
+        <Button title="Click Me" styles="bg-blue-500 hover:bg-blue-700 rounded-sm rounded-lg rounded-full"/>
+      <Button title="Secondary Button" styles="bg-gray-500 hover:bg-gray-700 rounded-sm rounded-lg rounded-full" />
   
       </div>
       
