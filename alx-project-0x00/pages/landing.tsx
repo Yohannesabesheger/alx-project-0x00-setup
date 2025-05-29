f48c593979d039533b26b9f80c6c572dda2005dd
yohannesm <jonmersha@gmail.com>
2025-05-29 09:11:40 +0300

    final confing

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,16 @@
+import Button from "@/components/Button"
+import Card from "@/components/Card"
+
 const Landing: React.FC =  () => {
     return (
       <div>
         <h1 className=" text-xl font-extralight">Landing Page</h1>
+        <Card />
+        <Button title="Click Me" styles="bg-blue-500 hover:bg-blue-700"  />
+      <Button title="Secondary Button" styles="bg-gray-500 hover:bg-gray-700" />
+  
       </div>
+      
     )
   }
   export default Landing
